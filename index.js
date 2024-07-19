e1825207b51c01a8d837d6b7313fd5ccbcfabfb2
Michael-star26 <130248327+Michael-star26@users.noreply.github.com>
2024-07-19 22:02:56 +0300

    Second commit,,,the fortune moduke and page testing

--- index.js
+++ index.js
@@ -4,17 +4,9 @@ var app=express();
 var handlebars=require('express3-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
-
+var fortune=require('./lib/fortune.js');
 // set the port
 app.set('port',process.env.PORT||3000);
-
-var fortunes=[
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple."
-];
 
 app.use(express.static(__dirname + '/public'));
 
@@ -23,8 +15,7 @@ app.get('/',function(req,res){
 });
 
 app.get('/about',function(req,res){
-    var randomFortune =fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+    res.render('about', { fortune: fortune.getFortune() });
 });
 
 
@@ -45,3 +36,5 @@ app.use(function(err,req,res,next){
 app.listen(app.get('port'),function(){
     console.log('Express started on http://localhost:'+app.get('port')+' Press Ctrl+C to terminate');
 });
+
+// git add -A adds changes made to all files
